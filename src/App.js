3bc92b36877e955a142112143df1092b1d2059be
Jose Luis Rodriguez <josesietepicos@gmail.com>
2021-04-06 14:05:17 +0200

    components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React from "react";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
+import VisibilityFilters from "./components/VisibilityFilters";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Todo List</h1>
+      <AddTodo />
+      <TodoList />
+      <VisibilityFilters />
     </div>
   );
 }
--- src/redux/actionTypes.js
+++ src/redux/actionTypes.js
@@ -1,1 +1,4 @@
-export const 
+export const ADD_TODO = "ADD_TODO";
+export const TOGGLE_TODO = "TOGGLE_TODO";
+export const SET_FILTER = "SET_FILTER";
+
